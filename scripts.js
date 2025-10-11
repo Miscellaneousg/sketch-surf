7c5e7253594af0559d2ef9038485a878614c8990
Sahil Yadav <sahil15785@gmail.com>
2025-10-11 19:17:13 +0000

    Finish raw grid square divs with javascript
    - Structure is down, need to add event to
      change state.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 const container = document.querySelector("#container");
 const fragment = document.createDocumentFragment();
-for(let i = 0; i<4;i++){
-    const div = document.createElement("div")
-    div.classList.add("pixel");
+let cellCount = 16;
+for(let i = 0; i<cellCount;i++){
+    const div = document.createElement("div");
+    div.classList.add("pixel-grid");
+    div.setAttribute("id",`column${i+1}`);
     fragment.appendChild(div);
 }
 container.appendChild(fragment);
+for(let i = 0;i<cellCount*cellCount;i++){
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    const columnNumber = i%cellCount;
+    const column = document.querySelector(`#column${columnNumber+1}`);
+    column.appendChild(div);
+}
