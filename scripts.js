0293b222f66aea176230bcc1fb69a6ca7bfff49a
Sahil Yadav <sahil15785@gmail.com>
2025-10-11 16:38:18 +0000

    Define pixel class for canvas resolution
    - Give some structure to add a number of pixels
    - Provide border through css (for debug purposes)

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+const fragment = document.createDocumentFragment();
+for(let i = 0; i<4;i++){
+    const div = document.createElement("div")
+    div.classList.add("pixel");
+    fragment.appendChild(div);
+}
+container.appendChild(fragment);
