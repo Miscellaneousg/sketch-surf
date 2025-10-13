84be33637eb86191361e8510aad67b9df20abd62
Sahil Yadav <sahil15785@gmail.com>
2025-10-13 08:25:46 +0000

    Add range slider to change grid
    - Change grid size with a easy to use slider
    - Invokes gridMaker function on each input event
    - Working on random color and opacity changer

--- scripts.js
+++ scripts.js
@@ -1,35 +1,46 @@
 const container = document.querySelector("#container");
-const fragment = document.createDocumentFragment();
-let cellCount = 100;
-for(let i = 0; i<cellCount;i++){
-    const div = document.createElement("div");
-    div.classList.add("pixel-grid");
-    div.setAttribute("id",`column${i+1}`);
-    fragment.appendChild(div);
-}
-container.appendChild(fragment);
-for(let i = 0;i<cellCount*cellCount;i++){
-    const div = document.createElement("div");
-    div.classList.add("cell");
-    const columnNumber = i%cellCount;
-    const column = document.querySelector(`#column${columnNumber+1}`);
-    column.appendChild(div);
-}
+const gridSizeInput = document.querySelector("input.grid-size");
 
-let isMouseDown = false;
+gridMaker();
 
-document.addEventListener("mousedown", ()=>isMouseDown=true);
-document.addEventListener("mouseup", ()=>isMouseDown=false);
+//set grid size 
+gridSizeInput.addEventListener("input", ()=>{
+  container.replaceChildren(); 
+  gridMaker();
+  document.querySelector("label.grid-size").textContent=`GRID: ${gridSizeInput.value}`
+  });
 
+function gridMaker(){
+  const fragment = document.createDocumentFragment();
+  let cellCount = gridSizeInput.value;
+
+  for(let i = 0; i<cellCount;i++){
+      const div = document.createElement("div");
+      div.classList.add("pixel-grid");
+      div.setAttribute("id",`column${i+1}`);
+      fragment.appendChild(div);
+  }
+
+  container.appendChild(fragment);
+
+  for(let i = 0;i<cellCount*cellCount;i++){
+      const div = document.createElement("div");
+      div.classList.add("cell");
+      const columnNumber = i%cellCount;
+      const column = document.querySelector(`#column${columnNumber+1}`);
+      column.appendChild(div);
+  }
+};
+
+container.addEventListener("dragstart", (e) => e.preventDefault());
 // Use delegation for clicks and hovers
 container.addEventListener("mousedown", (e) => {
   if (e.target.classList.contains("cell")) {
     e.target.classList.add("active");
   }
 });
-
 container.addEventListener("mousemove", (e) => {
-  if (isMouseDown && e.target.classList.contains("cell")) {
+  if (e.buttons === 1 && e.target.classList.contains("cell")) {
     e.target.classList.add("active");
   }
 });
